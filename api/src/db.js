e2a18ab4a444bfd1ecc97e265e4ee9c03e67ba5e
joshuaavalo <avalojoshua83@gmail.com>
2024-08-25 10:41:35 -0300

    prueba deploy2

--- api/src/db.js
+++ api/src/db.js
@@ -2,8 +2,26 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_URL } = process.env;
 
+
+
+const sequelize = new Sequelize(
+   `${DB_URL}`,
+   {
+     logging: false, // set to console.log to see the raw SQL queries
+     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+     dialectOptions: {
+       ssl: {
+         require: true,
+         rejectUnauthorized: false, // Puedes establecer esto en true si tienes un certificado SSL válido
+       },
+     },
+
+   }
+
+); 
+/*
 const sequelize = new Sequelize(
    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
    {
@@ -11,6 +29,7 @@ const sequelize = new Sequelize(
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
    }
 );
+*/
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
